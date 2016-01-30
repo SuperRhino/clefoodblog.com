ee9187430117f0896ff7b77a4245d7946244a0d9
Pasto <rpastorelle@yahoo.com>
2016-01-30 16:07:18 -0500

    Better AddPageForm

--- resources/js/Components/AddPageForm.js
+++ resources/js/Components/AddPageForm.js
@@ -10,16 +10,15 @@ export default class AddPageForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.eventTimestamp = null;
+    this.postTimestamp = null;
 
     this.state = {
       authorized: true,
       user: this.props.user,
     };
 
-    this._onSubmitEvent = this._onSubmitEvent.bind(this);
     this._onSubmitPage = this._onSubmitPage.bind(this);
-    this._onEventDateChange = this._onEventDateChange.bind(this);
+    this._onPostDateChange = this._onPostDateChange.bind(this);
     this._onUserChange = this._onUserChange.bind(this);
   }
 
@@ -40,99 +39,104 @@ export default class AddPageForm extends React.Component {
   render() {
     if (! this.state.authorized) return <h4>Must be logged in :(</h4>;
 
-    let dateInputProps = {placeholder: "Event Date/Time"},
-        placeholderCopy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis est lectus, posuere scelerisque massa rhoncus in. Vestibulum facilisis purus non velit porttitor, non imperdiet erat condimentum.";
-
+    let placeholderCopy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi iaculis est lectus, posuere scelerisque massa rhoncus in. Vestibulum facilisis purus non velit porttitor, non imperdiet erat condimentum.";
 
     // add .has-success or .has-error
     // .glyphicon-ok or .glyphicon-remove
     return (
-      <div className="row">
-        <div className="col-xs-4">
-          <h1>Add Event</h1>
-          <p className="lead"><em>Add an event to the homepage.</em></p>
-          <form className="" role="form" onSubmit={this._onSubmitEvent}>
-            <div className="form-group">
-                <input ref="eventTitle" className="form-control input-lg" type="text" placeholder="Headline" />
-            </div>
-            <div className="form-group">
-              <textarea ref="eventDescription" className="form-control input-lg" rows="3" placeholder="Enter short description"></textarea>
-            </div>
-            <div className="form-group">
-              <input ref="eventDetails" className="form-control input-lg" type="text" placeholder="http://event.com/details" />
-            </div>
+      <form className="" role="form" onSubmit={this._onSubmitPage}>
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>Add New Page</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-4">
+            <p className="lead"><em>Add meta data:</em></p>
+          </div>
+          <div className="col-xs-8">
+            <p className="lead"><em>Write article:</em></p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-4">
             <div className="form-group">
               <DateTimeInput
                 size={'lg'}
                 defaultText={''}
-                inputProps={dateInputProps}
+                inputProps={{placeholder: "Article Date/Time"}}
                 format={'x'}
                 inputFormat={'YYYY-MM-DD hh:mm A'}
-                onChange={this._onEventDateChange} />
+                onChange={this._onPostDateChange} />
             </div>
-            <button type="submit" className="btn btn-lg btn-info" style={styles.button}>
-              <span className="glyphicon glyphicon-ok-sign"></span>
-              {' Add Event'}
-            </button>
-          </form>
-        </div>
-        <div className="col-xs-8">
-          <h1>Add Page</h1>
-          <p className="lead"><em>Optionally add details page.</em></p>
-          <form className="" role="form" onSubmit={this._onSubmitPage}>
+            <div className="form-group">
+              <input ref="pageUri" className="form-control input-lg" type="text" placeholder="page-url" />
+            </div>
+            <div className="form-group">
+              <select ref="pageCategory" className="form-control input-lg">
+                <option value="">— Choose Category —</option>
+                <option>Reviews</option>
+                <option>Recipes</option>
+                <option>How To's</option>
+                <option>Friday Food Fight</option>
+                <option>Where To Find...</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <div style={styles.previewImage}>Drag Preview Image Here</div>
+            </div>
+            <div className="form-group">
+              <input ref="metaTitle" className="form-control input-lg" type="text" placeholder="Enter meta title" />
+            </div>
+            <div className="form-group">
+              <textarea ref="metaDescription" className="form-control input-lg" rows="2" placeholder="Enter meta description"></textarea>
+            </div>
+            <div className="form-group">
+              <input ref="metaKeywords" className="form-control input-lg" type="text" placeholder="Enter meta keywords" />
+            </div>
+          </div>
+          <div className="col-xs-8">
             <div className="form-group">
                 <input ref="pageTitle" className="form-control input-lg" type="text" placeholder="Headline" />
             </div>
             <div className="form-group">
-              <textarea ref="pageDescription" className="form-control input-lg" rows="5" placeholder={placeholderCopy}></textarea>
+              <textarea ref="pageArticle" className="form-control input-lg" rows="10" placeholder={placeholderCopy}></textarea>
             </div>
-            <button type="submit" className="btn btn-lg btn-success" style={styles.button}>
+            <button type="submit" className="btn btn-lg btn-info" style={styles.button}>
               <span className="glyphicon glyphicon-plus-sign"></span>
-              {' Add Event + Page'}
+              {' Add Page'}
             </button>
-          </form>
+          </div>
         </div>
-      </div>
+      </form>
     );
   }
 
-  _onEventDateChange(datetime) {
-    this.eventTimestamp = datetime;
+  _onPostDateChange(datetime) {
+    this.postTimestamp = datetime;
   }
 
-  _getEventDateFormat() {
-    return moment.unix(this.eventTimestamp / 1000).format("YYYY-MM-DD HH:mm:00");
-  }
-
-  _getEventData() {
-    return {
-      title: this.refs.eventTitle.value,
-      description: this.refs.eventDescription.value,
-      details_uri: this.refs.eventDetails.value,
-      event_date: this.eventTimestamp ? this._getEventDateFormat() : null,
-    };
+  _getPostDateFormat() {
+    return moment.unix(this.postTimestamp / 1000).format("YYYY-MM-DD HH:mm:00");
   }
 
   _getPageData() {
     return {
       title: this.refs.pageTitle.value,
-      description: this.refs.pageDescription.value,
+      article: this.refs.pageArticle.value,
+      uri: this.refs.pageUri.value,
+      category: this.refs.pageCategory.value,
+      meta_title: this.refs.metaTitle.value,
+      meta_description: this.refs.metaDescription.value,
+      meta_keywords: this.refs.metaKeywords.value,
+      post_date: this.postTimestamp ? this._getPostDateFormat() : null,
     };
   }
 
   _onSubmitPage(e) {
     e.preventDefault();
-    var data = {
-      event: this._getEventData(),
-      page: this._getPageData(),
-    };
+    var data = this._getPageData();
     console.log('_onSubmitPage', data);
-  }
-
-  _onSubmitEvent(e) {
-    e.preventDefault();
-    var data = this._getEventData();
-    console.log('_onSubmitEvent', data);
   }
 
   _onUserChange(user) {
@@ -144,5 +148,14 @@ export default class AddPageForm extends React.Component {
 var styles = {
   button: {
     width: '100%',
+  },
+  previewImage: {
+    color: "rgb(150,150,150)",
+    borderWidth: 2,
+    borderColor: "rgba(150,150,150,.8)",
+    borderStyle: "dashed",
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: "40px 0",
   },
 };
