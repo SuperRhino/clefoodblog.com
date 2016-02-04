52669e72d210b4f30c2d5e1852f35c49c30125b0
Pasto <rpastorelle@yahoo.com>
2016-02-03 21:33:44 -0500

    Everything under one house (why use bower?)

--- gulp/config.js
+++ gulp/config.js
@@ -5,8 +5,8 @@ module.exports = {
   appCssFiles: './resources/less/*.less',
   prodFiles: './public/build/@(css|js)/*-*.*',
   commonCss: [
-    "./bower_components/bootstrap/dist/css/bootstrap.css",
-    "./bower_components/bootstrap/dist/css/bootstrap-theme.css",
+    "./node_modules/bootstrap/dist/css/bootstrap.css",
+    "./node_modules/bootstrap/dist/css/bootstrap-theme.css",
     "./node_modules/react-bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css",
     "./node_modules/react-medium-editor/node_modules/medium-editor/dist/css/medium-editor.css",
     "./node_modules/react-medium-editor/node_modules/medium-editor/dist/css/themes/default.css",
