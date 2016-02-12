45677e2ec35dbe582d9417968488c69d6a529a2a
Pasto <rpastorelle@yahoo.com>
2016-02-11 22:06:11 -0500

    Add file uploads

--- config/config.php
+++ config/config.php
@@ -24,6 +24,9 @@ $container = new Slim\Container([
         'app.paths.css'   => '/build/css',
         'app.assets'      => $assets,
 
+        'app.paths.upload_path' => '/public/uploads',
+        'app.paths.upload_dir'  => '/uploads',
+
         'hashids.salt'       => 'C7WPHfv6N73WxzNfDPDtGEHmzUKbbyE2',
         'hashids.min-length' => 3,
 
--- src/App/Controllers/Api/PageController.php
+++ src/App/Controllers/Api/PageController.php
@@ -28,8 +28,27 @@ class PageController extends BaseApiController
     public function uploadFile()
     {
         // $pageId = $this->params('pageId');
+        if (empty($_FILES)) {
+            throw new BadRequestException('Missing file upload');
+        }
+
+        $fileKey    = 'file';
+        $tmpFile    = $_FILES[$fileKey]['tmp_name'];
+        $targetName = basename($_FILES[$fileKey]['name']);
+        $targetPath = $this->app->getSetting('base_path') . $this->app->getSetting('app.paths.upload_path');
+        $targetFile = $targetPath .'/'. $targetName;
+        $fileUri    = $this->app->getSetting('app.paths.upload_dir') .'/'. $targetName;
+
+        if (! move_uploaded_file($tmpFile, $targetFile)) {
+            // Possible file upload attack!!
+            throw new BadRequestException('Upload failed');
+        }
+
+        // File is valid, and was successfully uploaded
+        // TODO Insert Into DB
+
         return $this->success([
-            'FILES' => $_FILES,
+            'file_uri' => $fileUri,
         ]);
     }
 }
